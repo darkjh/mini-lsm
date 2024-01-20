cf3cec0695be85fe74a82a193cad0d2ecb56a45a
Han JU <han.ju@datadoghq.com>
2024-01-21 00:24:44 +0100

    Impl block builder

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -1,12 +1,10 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 mod builder;
 mod iterator;
 
 pub use builder::BlockBuilder;
 /// You may want to check `bytes::BufMut` out when manipulating continuous chunks of memory
-use bytes::Bytes;
+use bytes::{Buf, Bytes};
+use bytes::BufMut;
 pub use iterator::BlockIterator;
 
 /// A block is the smallest unit of read and caching in LSM tree.
@@ -27,12 +25,32 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the tutorial
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let mut buf = self.data.clone();
+        for offset in &self.offsets {
+            buf.put_u16(*offset);
+        }
+        let num_of_elements = self.offsets.len() as u16;
+        buf.put_u16(num_of_elements);
+        buf.into()
     }
 
+    // TODO check bytes and its From/Into typeclasses
+    // TODO check how bytes position works, why data need to be extracted after offsets
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let num_of_elements = (&data[data.len() - 2..]).get_u16() as usize;
+        let data_end = data.len() - 2 - num_of_elements * 2;
+
+        let offsets = &(data[data_end..data.len()-2])
+            .chunks(2)
+            .map(|mut x| x.get_u16())
+            .collect::<Vec<u16>>();
+        let data = (&data[0..data_end]).to_vec();
+
+        Block {
+            data,
+            offsets: offsets.to_vec(),
+        }
     }
 }
 
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -1,30 +1,59 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use super::Block;
 
 /// Builds a block.
-pub struct BlockBuilder {}
+pub struct BlockBuilder {
+    block_size: usize,
+    current_size: usize,
+    kvs: Vec<(Vec<u8>, Vec<u8>)>
+}
 
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        BlockBuilder {
+            block_size,
+            current_size: 0,
+            kvs: Vec::new()
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: &[u8], value: &[u8]) -> bool {
-        unimplemented!()
+        // key_len + value_len + offset_len(2 bytes) + sizes * 2 (2 bytes each)
+        let size = key.len() + value.len() + 6;
+        if self.current_size + size > self.block_size {
+            return false;
+        }
+
+        self.current_size += size;
+        self.kvs.push((key.to_vec(), value.to_vec()));
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.kvs.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        let mut data = Vec::new();
+        let mut offsets = Vec::new();
+        let mut current_offset = 0u16;
+        for (key, value) in self.kvs {
+            let key_len = u16::try_from(key.len()).unwrap();
+            let value_len = u16::try_from(value.len()).unwrap();
+            data.extend_from_slice(&key_len.to_ne_bytes());
+            data.extend_from_slice(&key);
+            data.extend_from_slice(&value_len.to_ne_bytes());
+            data.extend_from_slice(&value);
+            offsets.push(current_offset);
+            current_offset += key_len + value_len + 4;
+        }
+        Block {
+            data,
+            offsets
+        }
     }
 }
