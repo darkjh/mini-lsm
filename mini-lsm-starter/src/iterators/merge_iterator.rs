97c34c044cafa90445ac03a68d4a50e9318f0aa2
Han JU <han.ju@datadoghq.com>
2024-02-04 22:08:26 +0100

    Iterator: impl merge iterator

--- mini-lsm-starter/src/iterators/merge_iterator.rs
+++ mini-lsm-starter/src/iterators/merge_iterator.rs
@@ -1,7 +1,5 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use std::cmp::{self};
+use std::collections::binary_heap::PeekMut;
 use std::collections::BinaryHeap;
 
 use anyhow::Result;
@@ -10,7 +8,10 @@ use crate::key::KeySlice;
 
 use super::StorageIterator;
 
-struct HeapWrapper<I: StorageIterator>(pub usize, pub Box<I>);
+struct HeapWrapper<I: StorageIterator> {
+    pub gen: usize,
+    pub iter: Box<I>,
+}
 
 impl<I: StorageIterator> PartialEq for HeapWrapper<I> {
     fn eq(&self, other: &Self) -> bool {
@@ -23,10 +24,10 @@ impl<I: StorageIterator> Eq for HeapWrapper<I> {}
 impl<I: StorageIterator> PartialOrd for HeapWrapper<I> {
     #[allow(clippy::non_canonical_partial_ord_impl)]
     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-        match self.1.key().cmp(&other.1.key()) {
+        match self.iter.key().cmp(&other.iter.key()) {
             cmp::Ordering::Greater => Some(cmp::Ordering::Greater),
             cmp::Ordering::Less => Some(cmp::Ordering::Less),
-            cmp::Ordering::Equal => self.0.partial_cmp(&other.0),
+            cmp::Ordering::Equal => self.gen.partial_cmp(&other.gen),
         }
         .map(|x| x.reverse())
     }
@@ -47,7 +48,38 @@ pub struct MergeIterator<I: StorageIterator> {
 
 impl<I: StorageIterator> MergeIterator<I> {
     pub fn create(iters: Vec<Box<I>>) -> Self {
-        unimplemented!()
+        if iters.is_empty() {
+            return MergeIterator::invalid();
+        }
+
+        let mut gen = 0;
+        let mut heap = BinaryHeap::with_capacity(iters.len());
+
+        for iter in iters {
+            if iter.is_valid() {
+                let item = HeapWrapper { gen, iter };
+                gen += 1;
+                heap.push(item);
+            }
+        }
+
+        // all iters are invalid
+        if heap.is_empty() {
+            return MergeIterator::invalid();
+        }
+
+        let current = heap.pop();
+        MergeIterator {
+            iters: heap,
+            current,
+        }
+    }
+
+    fn invalid() -> Self {
+        MergeIterator {
+            iters: BinaryHeap::new(),
+            current: None,
+        }
     }
 }
 
@@ -57,18 +89,57 @@ impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> StorageIt
     type KeyType<'a> = KeySlice<'a>;
 
     fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.current.as_ref().unwrap().iter.key()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.current.as_ref().unwrap().iter.value()
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.current.is_some()
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        // if there are the same key in other iterators, drop them
+        let current_key = self.current.as_ref().unwrap().iter.key();
+        while let Some(mut peek) = self.iters.peek_mut() {
+            if peek.iter.key() == current_key {
+                match peek.iter.next() {
+                    Ok(_) => {
+                        if !peek.iter.is_valid() {
+                            // remove invalid iterator from the heap
+                            PeekMut::pop(peek);
+                        }
+                    }
+                    e @ Err(_) => return e,
+                }
+            } else {
+                break;
+            }
+        }
+
+        let mut current_iter = self.current.take().unwrap();
+        match current_iter.iter.next() {
+            Ok(_) => {
+                // make sure only valid iterator is pushed back into the heap
+                if current_iter.iter.is_valid() {
+                    self.iters.push(current_iter);
+                }
+            }
+            e @ Err(_) => return e,
+        }
+
+        let mut next = self.iters.pop().take();
+        while next.is_some() {
+            let n = next.unwrap();
+            if n.iter.is_valid() {
+                let _ = self.current.insert(n);
+                break;
+            } else {
+                next = self.iters.pop().take();
+            }
+        }
+        Ok(())
     }
 }
