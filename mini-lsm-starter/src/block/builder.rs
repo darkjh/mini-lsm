28943d0668e4037ccced9505b8000112ee7e7e71
Han JU <han.ju@datadoghq.com>
2024-02-08 00:15:26 +0100

    Block: impl block builder, encode, decode and iterator

--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -1,7 +1,5 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use crate::key::{KeySlice, KeyVec};
+use bytes::BufMut;
 
 use super::Block;
 
@@ -20,22 +18,43 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        BlockBuilder {
+            offsets: vec![],
+            data: vec![],
+            block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        let current_size = self.data.len() + self.offsets.len() * 2;
+
+        // key_len + value_len + offset_len(2 bytes) + sizes * 2 (2 bytes each)
+        let size = key.len() + value.len() + 6;
+        if current_size + size > self.block_size && !self.data.is_empty() {
+            return false;
+        }
+
+        self.offsets.push(self.data.len() as u16);
+        self.data.put_u16(key.len() as u16);
+        self.data.put(key.into_inner());
+        self.data.put_u16(value.len() as u16);
+        self.data.put(value);
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.data.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
--- mini-lsm-starter/src/block/iterator.rs
+++ mini-lsm-starter/src/block/iterator.rs
@@ -1,6 +1,4 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
+use bytes::Buf;
 use std::sync::Arc;
 
 use crate::key::{KeySlice, KeyVec};
@@ -34,44 +32,84 @@ impl BlockIterator {
 
     /// Creates a block iterator and seek to the first entry.
     pub fn create_and_seek_to_first(block: Arc<Block>) -> Self {
-        unimplemented!()
+        let mut iter = BlockIterator::new(block);
+        iter.seek_to_first();
+        iter
     }
 
     /// Creates a block iterator and seek to the first key that >= `key`.
     pub fn create_and_seek_to_key(block: Arc<Block>, key: KeySlice) -> Self {
-        unimplemented!()
+        let mut iter = BlockIterator::new(block);
+        iter.seek_to_key(key);
+        iter
     }
 
     /// Returns the key of the current entry.
     pub fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.key.as_key_slice()
     }
 
     /// Returns the value of the current entry.
     pub fn value(&self) -> &[u8] {
-        unimplemented!()
+        &self.block.data[self.value_range.0..self.value_range.1]
     }
 
     /// Returns true if the iterator is valid.
     /// Note: You may want to make use of `key`
     pub fn is_valid(&self) -> bool {
-        unimplemented!()
+        !self.key.is_empty()
     }
 
     /// Seeks to the first key in the block.
     pub fn seek_to_first(&mut self) {
-        unimplemented!()
+        self.seek_to_idx(0);
     }
 
     /// Move to the next key in the block.
     pub fn next(&mut self) {
-        unimplemented!()
+        self.seek_to_idx(self.idx + 1);
+    }
+
+    fn seek_to_idx(&mut self, idx: usize) {
+        if idx >= self.block.offsets.len() {
+            self.key.clear();
+            return;
+        }
+        let offset = self.block.offsets[idx];
+        self.seek_to_offset(offset);
+        self.idx = idx;
+    }
+
+    fn seek_to_offset(&mut self, offset: u16) {
+        let mut entry = &self.block.data[offset as usize..];
+
+        let key_len = entry.get_u16() as usize;
+        self.key = KeyVec::from_vec(entry[..key_len].to_vec());
+
+        entry.advance(key_len);
+
+        let value_len = entry.get_u16() as usize;
+        self.value_range.0 = offset as usize + 4 + key_len;
+        self.value_range.1 = self.value_range.0 + value_len;
     }
 
     /// Seek to the first key that >= `key`.
     /// Note: You should assume the key-value pairs in the block are sorted when being added by
     /// callers.
     pub fn seek_to_key(&mut self, key: KeySlice) {
-        unimplemented!()
+        let pos = self.block.offsets.binary_search_by(|v| {
+            let mut entry = &self.block.data[*v as usize..];
+            let key_len = entry.get_u16();
+            let entry_key = &entry[..key_len as usize];
+            entry_key.cmp(key.into_inner())
+        });
+
+        match pos {
+            Ok(pos) => self.seek_to_idx(pos),
+            Err(pos) if pos < self.block.offsets.len() => self.seek_to_idx(pos),
+            _ => {
+                self.key.clear();
+            }
+        }
     }
 }
