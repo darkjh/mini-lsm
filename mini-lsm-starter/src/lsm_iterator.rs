4351a1bd87b2f451ed697fd25d6e8917a6003986
Han JU <han.ju@datadoghq.com>
2024-02-25 13:44:01 +0100

    Read path: hook sstable scan with lower bound

--- mini-lsm-starter/src/lsm_iterator.rs
+++ mini-lsm-starter/src/lsm_iterator.rs
@@ -1,13 +1,17 @@
 use anyhow::{bail, Result};
 
+use crate::iterators::two_merge_iterator::TwoMergeIterator;
+use crate::table::SsTableIterator;
 use crate::{
     iterators::{merge_iterator::MergeIterator, StorageIterator},
     mem_table::MemTableIterator,
 };
 
 /// Represents the internal type for an LSM iterator. This type will be changed across the tutorial for multiple times.
-type LsmIteratorInner = MergeIterator<MemTableIterator>;
+type LsmIteratorInner =
+    TwoMergeIterator<MergeIterator<MemTableIterator>, MergeIterator<SsTableIterator>>;
 
+// TODO handle end bound here
 pub struct LsmIterator {
     inner: LsmIteratorInner,
 }
