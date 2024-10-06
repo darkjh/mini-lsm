73744f14c419f3a6f40e0fe1accb07ce842159c8
Han JU <han.ju@datadoghq.com>
2024-10-06 22:58:06 +0200

    Impl WAL checksum

--- mini-lsm-starter/src/wal.rs
+++ mini-lsm-starter/src/wal.rs
@@ -3,7 +3,7 @@ use std::io::{BufWriter, Read, Write};
 use std::path::Path;
 use std::sync::Arc;
 
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 use bytes::{Buf, BufMut, Bytes};
 use crossbeam_skiplist::SkipMap;
 use parking_lot::Mutex;
@@ -39,12 +39,23 @@ impl Wal {
 
         let mut buf = buf.as_slice();
         while buf.has_remaining() {
+            let mut hasher = crc32fast::Hasher::new();
+            hasher.update(&buf[..2]);
             let key_len = buf.get_u16() as usize;
+            hasher.update(&buf[..key_len]);
             let key = Bytes::copy_from_slice(&buf[..key_len]);
             buf.advance(key_len);
+
+            hasher.update(&buf[..2]);
             let value_len = buf.get_u16() as usize;
+            hasher.update(&buf[..value_len]);
             let value = Bytes::copy_from_slice(&buf[..value_len]);
             buf.advance(value_len);
+
+            let checksum = buf.get_u32();
+            if checksum != hasher.finalize() {
+                bail!("WAL record checksum mismatch");
+            }
 
             skiplist.insert(key, value);
         }
@@ -57,12 +68,17 @@ impl Wal {
 
     pub fn put(&self, key: &[u8], value: &[u8]) -> Result<()> {
         let mut writer = self.file.lock();
-        let mut buf: Vec<u8> =
-            Vec::with_capacity(key.len() + value.len() + std::mem::size_of::<u16>() * 2);
+        let data_size = key.len() + value.len() + std::mem::size_of::<u16>() * 2;
+        // data (data_size) | checksum (4B)
+        let mut buf: Vec<u8> = Vec::with_capacity(data_size + 4);
         buf.put_u16(key.len() as u16);
         buf.put(key);
         buf.put_u16(value.len() as u16);
         buf.put(value);
+
+        let end = buf.len();
+        let checksum = crc32fast::hash(&buf[..end]);
+        buf.put_u32(checksum);
 
         writer.write_all(&buf).context("failed to write to WAL")?;
         Ok(())
