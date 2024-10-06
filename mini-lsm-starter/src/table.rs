c1d91fe32c845a8f203fec06ce1560e71707af61
Han JU <han.ju@datadoghq.com>
2024-10-06 18:44:20 +0200

    Impl block metadata checksum

--- mini-lsm-starter/src/table.rs
+++ mini-lsm-starter/src/table.rs
@@ -32,7 +32,9 @@ impl BlockMeta {
     /// You may add extra fields to the buffer,
     /// in order to help keep track of `first_key` when decoding from the same buffer in the future.
     pub fn encode_block_meta(block_meta: &[BlockMeta], buf: &mut Vec<u8>) {
+        // number of blocks
         buf.put_u32(block_meta.len() as u32);
+        let meta_start = buf.len();
         // offset (4B) | fkey_len (2B) | fkey | lkey_len (2B) | lkey
         for meta in block_meta {
             buf.put_u32(meta.offset as u32);
@@ -41,10 +43,22 @@ impl BlockMeta {
             buf.put_u16(meta.last_key.len() as u16);
             buf.put(meta.last_key.raw_ref());
         }
+        let meta_end = buf.len();
+        let checksum = crc32fast::hash(&buf[meta_start..meta_end]);
+
+        // #blocks (4B) | metas (varlen) | checksum (4B)
+        buf.put_u32(checksum);
     }
 
     /// Decode block meta from a buffer.
-    pub fn decode_block_meta(buf: &mut impl Buf) -> Vec<BlockMeta> {
+    pub fn decode_block_meta(mut buf: &[u8]) -> Result<Vec<BlockMeta>> {
+        let end_offset = buf.len();
+        let checksum = (&buf[end_offset - 4..]).get_u32();
+        // first 4B is the number of blocks, metadata is after that
+        if checksum != crc32fast::hash(&buf[4..end_offset - 4]) {
+            bail!("Block metadata checksum mismatch");
+        }
+
         let size = buf.get_u32() as usize;
         let mut metas = Vec::with_capacity(size);
         for _ in 0..size {
@@ -61,7 +75,7 @@ impl BlockMeta {
             };
             metas.push(meta);
         }
-        metas
+        Ok(metas)
     }
 }
 
@@ -138,7 +152,7 @@ impl SsTable {
             block_meta_offset,
             bloom_filter_offset - block_meta_offset - 4,
         )?);
-        let block_meta = BlockMeta::decode_block_meta(&mut block_meta_bytes);
+        let block_meta = BlockMeta::decode_block_meta(&mut block_meta_bytes)?;
 
         let first_key = KeyBytes::from_bytes(Bytes::copy_from_slice(
             block_meta.first().unwrap().first_key.raw_ref(),
@@ -200,7 +214,11 @@ impl SsTable {
         // TODO could do one single read for both data and checksum to improve performance
         let data = &self.file.read(block_start, data_end - block_start)?;
         if crc32fast::hash(data) != Bytes::from(self.file.read(data_end, 4)?).get_u32() {
-            bail!("Checksum mismatch for sst {}, block {}", self.id, block_idx);
+            bail!(
+                "Block checksum mismatch for sst {}, block {}",
+                self.id,
+                block_idx
+            );
         }
 
         Ok(Arc::new(Block::decode(data)))
